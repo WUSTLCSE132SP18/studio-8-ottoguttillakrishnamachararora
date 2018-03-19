86d365a1ecf663208491d3eaf2a6a4521b13382c
matthewotto <matthewotto@matthews-mbp.dhcp.wustl.edu>
2018-03-19 14:24:04 -0500

    Otto, Krishnamachar, Arora, Guttilla Studio 8

---
+++ steps/steps.ino
@@ -0,0 +1,27 @@
+#include <Wire.h> // Must include Wire library for I2C
+#include <SparkFun_MMA8452Q.h> // Includes the SFE_MMA8452Q library
+
+MMA8452Q accel;
+
+void setup(){
+  Serial.begin(9600);
+  accel.init();
+}
+
+void loop(){
+  if (accel.available()){
+    accel.read();
+    printCalculatedAccels();
+  }
+}
+
+void printCalculatedAccels()
+{ 
+  Serial.print(accel.cx, 3);
+  Serial.print(",");
+  Serial.print(accel.cy, 3);
+  Serial.print(",");
+  Serial.print(accel.cz, 3);
+  Serial.println("");
+}
+
